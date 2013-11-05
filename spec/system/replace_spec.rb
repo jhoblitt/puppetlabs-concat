9a4629364a3ade007c2daafa96c0c222bf4d3e8e
Joshua Hoblitt <jhoblitt@cpan.org>
2013-11-04 19:49:15 -0700

    add system tests for replacing symlinks and directories

--- spec/system/replace_spec.rb
+++ spec/system/replace_spec.rb
@@ -1,9 +1,15 @@
 require 'spec_helper_system'
 
+describe 'file replacement' do
+  context 'file should not replace' do
+    before(:all) do
+      shell('echo "file exists" >> /tmp/file')
+    end
+    after(:all) do
+      shell('rm -rf /tmp/concat /var/lib/puppet/concat')
+    end
 
-describe 'file should not replace' do
-  shell('echo "file exists" >> /tmp/file')
-  context 'should fail' do
+
     pp="
       concat { '/tmp/file':
         owner   => root,
@@ -33,5 +39,106 @@ describe 'file should not replace' do
       its(:exit_code) { should be_zero }
     end
 
+    describe file('/tmp/file') do
+      it { should be_file }
+      it { should contain 'file exists' }
+      it { should_not contain '1' }
+      it { should_not contain '2' }
+    end
+  end
+
+  context 'symlink' do
+    before(:all) do
+      shell('mkdir /tmp/concat')
+      shell('ln -s /tmp/concat/dangling /tmp/concat/file')
+    end
+    after(:all) do
+      shell('rm -rf /tmp/concat /var/lib/puppet/concat')
+    end
+
+    pp="
+      concat { '/tmp/concat/file': }
+
+      concat::fragment { '1':
+        target  => '/tmp/concat/file',
+        content => '1',
+      }
+    "
+
+    context puppet_apply(pp) do
+      its(:stderr) { should be_empty }
+      its(:exit_code) { should_not == 1 }
+      its(:refresh) { should be_nil }
+      its(:stderr) { should be_empty }
+      its(:exit_code) { should be_zero }
+    end
+
+    describe file('/tmp/concat/file') do
+      it { should be_file }
+      it { should contain '1' }
+    end
+  end
+
+  context 'directory' do
+    before(:all) do
+      shell('mkdir /tmp/concat')
+      shell('mkdir /tmp/concat/file')
+    end
+    after(:all) do
+      shell('rm -rf /tmp/concat /var/lib/puppet/concat')
+    end
+
+    pp="
+      concat { '/tmp/concat/file': }
+
+      concat::fragment { '1':
+        target  => '/tmp/concat/file',
+        content => '1',
+      }
+    "
+
+    context puppet_apply(pp) do
+      its(:stderr) { should =~ /change from directory to file failed/ }
+      its(:exit_code) { should_not == 1 }
+      its(:refresh) { should be_nil }
+      its(:stderr) { should =~ /change from directory to file failed/ }
+      its(:exit_code) { should_not == 1 }
+    end
+
+    describe file('/tmp/concat/file') do
+      it { should be_directory }
+    end
+  end
+
+  context 'directory' do
+    before(:all) do
+      shell('mkdir /tmp/concat')
+      shell('mkdir /tmp/concat/file')
+    end
+    after(:all) do
+      shell('rm -rf /tmp/concat /var/lib/puppet/concat')
+    end
+
+    pp="
+      concat { '/tmp/concat/file': }
+
+      concat::fragment { '1':
+        target  => '/tmp/concat/file',
+        content => '1',
+      }
+    "
+
+    context puppet_apply(pp) do
+      its(:stderr) { pending; should be_empty }
+      its(:exit_code) { should_not == 1 }
+      its(:refresh) { should be_nil }
+      its(:stderr) { pending; should be_empty }
+      its(:exit_code) { pending; should be_zero }
+    end
+
+    describe file('/tmp/concat/file') do
+      xit { should be_file }
+      xit { should contain '1' }
+    end
   end
 end
